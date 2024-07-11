49d9768fd70bf15beca7f6229ec42b1d80fc6678
thaboTheboss <93175702+thaboTheboss@users.noreply.github.com>
2024-07-11 18:14:09 +0200

    Added Home Page and menu page

---
+++ brian-stride/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu ' id='explore-menu'>
+      <h1>Explore our Shoes</h1>
+      <p className='explore-menu-text'>Explore Our Expansive Collection: Step Into a World of Endless Possibilities</p>
+      <div className="explore-menu-list">
+      {menu_list.map((item,index) =>{
+        return (
+          <div key = {index}className='explore-menu-list-item'>
+            <img src={item.menu_image} alt=''/>
+            <p>{item.menu_name}</p>
+          </div>
+        )
+      })}      
+    </div>
+    <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
